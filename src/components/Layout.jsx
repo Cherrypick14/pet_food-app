ad9ea66cf7e3b841b83f4141c2b65464fc03d6b1
Cherrypick14 <cherrypicklet@gmail.com>
2022-05-22 07:56:09 +0300

    add layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Topbar from './Topbar'
+import PetSec from './PetSec'
+
+const Layout = () => {
+  return <>
+  
+  <Header />
+  <Topbar />
+  <PetSec />
+
+  </>
+}
+
+export default Layout
